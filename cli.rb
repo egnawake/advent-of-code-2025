40bcadb867bd4f40320567b7c481d5530a74db8b
João Inácio <egnawake@gmail.com>
2026-01-13 22:39:59 +0000

    Factor out CLI code

--- cli.rb
+++ cli.rb
@@ -1,27 +1,36 @@
-module CLI
-  def self.run(solver_class)
-    if ARGV.empty?
-      $stderr.puts "Argument error: no arguments"
-      exit 1
-    end
+class CLI
+  def initialize(solver_class)
+    @solver_class = solver_class
+  end
 
-    part = ARGV.shift
+  def start
+    part = parse!(ARGV, "part") { Integer(_1) }
 
     if ARGV.empty?
-      $stderr.puts "Argument error: no files to read"
-      exit 1
+      exit 1, "Argument error: input file"
     end
 
     case part
-    when "1"
-      solver = solver_class.new(ARGF)
+    when 1
+      solver = @solver_class.new(ARGF)
       puts solver.part_one
-    when "2"
-      solver = solver_class.new(ARGF)
+    when 2
+      solver = @solver_class.new(ARGF)
       puts solver.part_two
     else
-      $stderr.puts "Argument error: unrecognized part"
-      exit 1
+      exit 1, "Argument error: unrecognized part"
     end
+  end
+
+  private def parse!(args, err_msg, &parser)
+    arg = args.shift
+    parser.call(arg)
+  rescue
+    exit 1, "Argument error: #{err_msg}"
+  end
+
+  private def exit(status, message="Error: status #{status}")
+    $stderr.puts message
+    super status
   end
 end
